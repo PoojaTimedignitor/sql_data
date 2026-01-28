c0bc7ceddde02349f3a52330db301c7caed0eb86
Pooja Jadhav <pooja@timedignitorbuilt.com>
2026-01-28 15:24:27 +0530

    done sql

---
+++ lib/screen/edit_data.dart
@@ -0,0 +1,101 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:sql_db/db/db_helper.dart';
+
+class EditData extends StatefulWidget {
+  final int id;
+
+  const EditData({required this.id, super.key});
+
+  @override
+  State<EditData> createState() => _EditDataState();
+}
+
+class _EditDataState extends State<EditData> {
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
+
+  void fetchData()async{
+    Map<String, dynamic>? data =  await DBHelper.getSingleData(widget.id);
+    if(data != null){
+      _nameController.text = data['name'];
+      _ageController.text = data['age'].toString();
+    }
+  }
+
+  void _updateData(BuildContext context)async{
+    Map<String, dynamic> data = {
+      'name' : _nameController.text,
+      'age' : _ageController.text,
+    };
+    int id = await DBHelper.updateData(widget.id, data);
+    log('Update $id');
+    Navigator.pop(context, true);
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _ageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                    hintText: 'Enter name',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12))),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: _ageController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    hintText: 'Enter age',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12))),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.black),
+                  onPressed: () {
+                    // setState(() {
+                    //   _updateData(context);
+                    // });
+                    _updateData(context);
+                    _nameController.clear();
+                    _ageController.clear();
+                  },
+                  child: const Text('Update Data')),
+            ],
+          ),
+        ),
+
+      ),
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,155 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:sql_db/db/db_helper.dart';
+import 'edit_data.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, dynamic>> dataList = [];
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+
+  @override
+  void initState() {
+    _fetchUsers();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _ageController.dispose();
+    super.dispose();
+  }
+
+  void _fetchUsers() async {
+    List<Map<String, dynamic>> userList = await DBHelper.getData();
+    setState(() {
+      dataList = userList;
+    });
+  }
+
+  void _saveData() async {
+    final name = _nameController.text;
+    final age = int.tryParse(_ageController.text) ?? 0;
+    int insertId = await DBHelper.insertUser(name, age);
+    log('$insertId');
+
+    List<Map<String, dynamic>> updateData = await DBHelper.getData();
+    setState(() {
+      dataList = updateData;
+    });
+  }
+
+  void _delete(int dataId) async {
+    int id = await DBHelper.deleteData(dataId);
+    List<Map<String, dynamic>> updateData = await DBHelper.getData();
+    setState(() {
+      dataList = updateData;
+    });
+
+    log('Delete :$id');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 60),
+        child: Column(
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                      hintText: 'Enter name',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: _ageController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      hintText: 'Enter age',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.black),
+                    onPressed: () {
+                      setState(() {
+                        _saveData();
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text('Data Add Successfully'),
+                          backgroundColor: Colors.green,
+                        ));
+                      });
+                      _nameController.clear();
+                      _ageController.clear();
+                    },
+                    child: const Text('Save Data')),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: dataList.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(dataList[index]['name']),
+                      subtitle: Text(dataList[index]['age'].toString()),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                              onPressed: () async {
+                                final result = await Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        EditData(id: dataList[index]['id']),
+                                  ),
+                                );
+
+                                if (result == true) {
+                                  _fetchUsers();
+                                }
+                              },
+                              icon: const Icon(Icons.edit)),
+                          IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _delete(dataList[index]['id']);
+                                });
+                              },
+                              icon: const Icon(Icons.delete))
+                        ],
+                      ),
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
